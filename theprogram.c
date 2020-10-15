519773e337a49061132fddf78fb87c7c0d2b7385
Mahima Hemachandra <nomad.inof@gmail.com>
2020-10-15 18:20:35 +0530

    Create theprogram.c

---
+++ theprogram.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+in x,y,count;
+float z,simp_int;
+clrscr();
+
+for(count=1;count<=3;count++)
+{
+print(“\n\nEnter the value for x, y & z\n\n”);
+scanf(“%d%d%f”,&x,&y,&z);
+
+simp_int=x*y*z/100;
+printf(“Simple Interest=%f”,simp_in);
+}
+getch();
+}
